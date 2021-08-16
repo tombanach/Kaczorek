2323f3f25345c78ed94c28a830592ded4aa8639f
tombanach <tom.banach98@gmail.com>
2021-08-16 20:51:42 +0200

    Add project files.

---
+++ Kaczorek.BL/Klient.cs
@@ -0,0 +1,31 @@
+﻿namespace Kaczorek.BL
+{
+    public class Klient
+    {
+        private string _nazwisko;
+
+        public string Nazwisko
+        {
+            get
+            {
+                return _nazwisko;
+            }
+            set
+            {
+                _nazwisko = value;
+            }
+        }
+
+        public string Imie { get; set; }
+        public string Email { get; set; }
+        public int KlientId { get; private set; }
+        public string ImieNazwisko
+        {
+            get
+            {
+                return Imie + "," + Nazwisko;
+            }
+        }
+
+    }
+}
